ebba86ef46640ab58dc8c6b880c2ce12a03f484a
dshryn <135850441+dshryn@users.noreply.github.com>
2024-05-15 15:48:03 +0530

    Update counter.dart

--- counter.dart
+++ counter.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatefulWidget {
+  @override
+  _MyWidgetState createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    counter = 0;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Widget'),
+      ),
+      body: Center(
+        child: Text('Counter- $counter'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: MyWidget(),
+  ));
+}
+
 
